d8df7ca6c10157fb4404ef65ad6fd373736de534
logoss <463010136@qq.com>
2020-03-15 23:10:04 +0800

    111

---
+++ Assets/Game/Scripts/ActorView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorView : MonoBehaviour
+{
+    public static ActorView mousePosActor;
+    public void onPointEnter()
+    {
+        ActorView.mousePosActor = this;
+    }
+    public void onPointExit()
+    {
+        if (ActorView.mousePosActor == this)
+        {
+            ActorView.mousePosActor = null;
+        }
+    }
+    //=========
+}
